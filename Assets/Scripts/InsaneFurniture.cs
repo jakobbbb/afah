8fdd0bf4e26060529c68910fe6cb74095238bb1f
Jakob Ruckel <jakob@ruckel.de>
2024-04-07 17:48:12 +0200

    insaneeeee

---
+++ Assets/Scripts/InsaneFurniture.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsaneFurniture : MonoBehaviour {
+    [SerializeField]
+    private Sprite m_Normal;
+    [SerializeField]
+    private Sprite m_Insane;
+    [SerializeField]
+    private Sprite m_InsaneInsaneAsFuck;
+}
