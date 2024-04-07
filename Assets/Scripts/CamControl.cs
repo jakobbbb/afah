5590bdf474f2d19c1ca1e9438556da490aab8c5f
Jakob Ruckel <jakob@ruckel.de>
2024-04-07 13:28:18 +0200

    oh god

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform m_MinIndoor;
+    [SerializeField]
+    private Transform m_MaxIndoor;
+    [SerializeField]
+    private Transform m_MinOutdoor;
+    [SerializeField]
+    private Transform m_MaxOutdoor;
+    void Update() {
+        bool indoor = true;
+        var min = indoor ? m_MinIndoor : m_MinOutdoor;
+        var max = indoor ? m_MaxIndoor : m_MaxOutdoor;
 
-    // Update is called once per frame
-    void Update()
-    {
         float x = Input.mousePosition.x / Screen.width;
         float lim = 0.2f;  // lol this looks like math
         float dx = 0f;
@@ -21,6 +25,10 @@ public class CamControl : MonoBehaviour
             dx = -speed_max * (lim -x );
         } else if (x > (1f-lim)) {
             dx = - speed_max * ((1f-lim) - x);
+        }
+        float new_x = transform.position.x + (Time.deltaTime * dx);
+        if (new_x < min.position.x || new_x > max.position.x) {
+            return;
         }
         transform.Translate(new Vector3(Time.deltaTime * dx, 0, 0));
     }
